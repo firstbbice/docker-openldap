95cff9747982df3a430a669e1d7dbdc8d76f9251
Brent Bice <bbice@bicetech.com>
2022-05-07 19:15:01 -0600

    Make entrypoint able to build slapd.d and/or openldap-data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+cd /opt/slapd 
+mkdir etc/slapd.d
+mkdir var/openldap-data
 
 # Check if we already have a config database and create one if we don't
 if [ ! -e /opt/slapd/etc/slapd.d/cn=config.ldif ]; then
-	cd /opt/slapd 
-
-	# I'm sometimes testing and starting without mounting optional volumes
-	# create these directories if they're not already there
-	mkdir -p /opt/slapd/etc/slapd.d
-	mkdir -p /opt/slapd/var/openldap-data
-
 	echo "Running: slapadd -n 0 -F etc/slapd.d -l etc/openldap/slapd.ldif"
 	slapadd -n 0 -F etc/slapd.d -l etc/openldap/slapd.ldif 2>&1
+fi
+
+if [ ! -e /opt/slapd/var/openldap-data/data.mdb ]; then
+	echo "Running: slapadd -F etc/slapd.d -b "dc=tcn,dc=com" -l etc/openldap/initial.ldif"
 	slapadd -F etc/slapd.d -b "dc=tcn,dc=com" -l etc/openldap/initial.ldif 2>&1
 fi
 
